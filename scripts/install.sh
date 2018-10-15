fd01653c8b87bccf11e7bacc1096c44b3560a3d8
wuhy <sparklewhy@gmail.com>
2018-10-15 18:15:44 +0800

    ci: fix ci error

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-npm i
+export NODE_VER=$(node -v)
+NODE_VER=${NODE_VER:1:2}
+NODE_VER=${NODE_VER/./}
+echo "NODE_VER=${NODE_VER}"
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+echo "BRANCH=${BRANCH}"
+GK=0
+if [[ "${BRANCH}" == greenkeeper/* ]]; then
+  GK=1
+fi
+echo npm install
+npm install
 npm run bootstrap
