782d6b4b9cdd64614f1f2985a0f6e52b450f3034
wuhy <sparklewhy@gmail.com>
2018-11-01 17:02:25 +0800

    chore(okam-build): up example

--- packages/okam-build/example/base/src/pages/todos/counter.vue
+++ packages/okam-build/example/base/src/pages/todos/counter.vue
@@ -1,12 +1,11 @@
 <template>
-    <view class="counter-wrap">
-        <view class="counter-title">Counter: {{counter}}</view>
-        <button plain class="counter-btn" @click="onAddCounter">增加 Counter</button>
-        <button plain class="counter-btn" @click="onMinusCounter">减少 Counter</button>
+    <view class="my-counter-wrap">
+        <counter @counterChange="handleCounterChange"></counter>
     </view>
 </template>
 
 <script>
+import Counter from '../../components/Counter';
 
 export default {
     config: { // The page config defined in page.json
@@ -14,55 +13,21 @@ export default {
         backgroundColor: '#fff'
     },
 
-    data: {
-    },
-
-    computed: {
-    },
-
-    $store: {
-        computed: ['counter'],
-        actions: {
-            addCounter(value = 1) {
-                console.log('add', value)
-                return {type: 'INCREMENT', value};
-            },
-            minusCounter(value = 1) {
-                return {type: 'DECREMENT', value};
-            }
-        }
-    },
-
-    created() {
-        let state = this.$store.getState();
-        console.log('created counter', state);
+    components: {
+        Counter
     },
 
     methods: {
-        onAddCounter() {
-            this.addCounter(5);
-        },
-
-        onMinusCounter() {
-            this.minusCounter();
+        handleCounterChange() {
+            console.log('counter change...', arguments)
         }
     }
 };
 </script>
 <style lang="stylus">
-.counter-wrap
+.my-counter-wrap
     min-height: 100vh
     padding: 20px
     box-sizing: border-box
     background: #fff
-
-    .counter-title
-        font-size: 16px
-        font-weight: 600
-        padding: 20px
-        text-align: center
-
-    .counter-btn
-        margin: 20px 0
-
 </style>
--- packages/okam-build/example/base/src/pages/vant/index.vue
+++ packages/okam-build/example/base/src/pages/vant/index.vue
@@ -1,11 +1,25 @@
 <template>
     <view class="vant-weapp-page">
-        <vant-button ref="btn" type="primary" @click="handleClick">按钮</vant-button>
+        <view>
+            <van-button ref="btn" type="primary" @click="handleClick">按钮</van-button>
+        </view>
+        <view>
+            <van-cell-group>
+                <van-field
+                    :value="userName"
+                    placeholder="请输入用户名"
+                    :border="false"
+                    @change="handleInputChange"
+                />
+            </van-cell-group>
+        </view>
     </view>
 </template>
 
 <script>
 import button from 'vant-weapp/dist/button';
+import fieldInput from 'vant-weapp/dist/field';
+import cellGroup from 'vant-weapp/dist/cell-group';
 
 export default {
     config: {
@@ -13,10 +27,13 @@ export default {
     },
 
     components: {
-        'vant-button': button
+        'van-button': button,
+        'van-input': fieldInput,
+        'van-cell-group': cellGroup
     },
 
     data: {
+        useName: '小李子'
     },
 
     mounted() {
@@ -27,11 +44,17 @@ export default {
         handleClick() {
             console.log('click', arguments);
             console.log(this)
+        },
+
+        handleInputChange() {
+            console.log('input changes', arguments);
         }
     }
 };
 </script>
 
 <style lang="stylus">
-
+.vant-weapp-page
+    margin: 20px
+    padding: 20px
 </style>
