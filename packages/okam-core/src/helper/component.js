3de4f62c660b20a7f9a26e30753786c00bebbde6
wuhy <sparklewhy@gmail.com>
2018-11-01 11:50:53 +0800

    wip(okam-core): delete okam extend props and mixins attributes when normalize

--- packages/okam-core/src/helper/component.js
+++ packages/okam-core/src/helper/component.js
@@ -18,10 +18,12 @@ export function normalizeAttributeNames(componentInfo) {
     let {props, properties, mixins, behaviors} = componentInfo;
 
     if (!properties && props) {
+        delete componentInfo.props;
         componentInfo.properties = normalizeProps(props);
     }
 
     if (!behaviors && mixins) {
+        delete componentInfo.mixins;
         componentInfo.behaviors = mixins;
     }
 
