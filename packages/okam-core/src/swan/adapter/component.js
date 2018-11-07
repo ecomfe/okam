d1fa2eab537a1b11639ebebba39e3ae18cdb1f1f
wuhy <sparklewhy@gmail.com>
2018-11-07 17:32:32 +0800

    feat(okam-core): add native swan component adapter to okam

---
+++ packages/okam-core/src/swan/adapter/component.js
@@ -0,0 +1,35 @@
+/**
+ * @file Fix swan native component to adapt okam event handler.
+ * @author sparklewhy@gmail.com
+ */
+
+'use strict';
+
+import {normalizeEventArgs} from '../../helper/triggerEvent';
+
+/**
+ * Emit custom component event
+ *
+ * @param  {...any} args the event arguments
+ */
+function emit(...args) {
+    // fix custom component event args
+    normalizeEventArgs(this, args);
+    this.triggerEvent.apply(this, args);
+}
+
+/**
+ * Make the native swan component to adapt to the okam framework
+ *
+ * @param {Object} component the component to adapt
+ * @return {Object}
+ */
+export default function adaptOKAM(component) {
+    let methods = component.methods || (component.methods = {});
+    if (!methods.$emit) {
+        methods.$emit = emit;
+    }
+    return component;
+}
+
+
