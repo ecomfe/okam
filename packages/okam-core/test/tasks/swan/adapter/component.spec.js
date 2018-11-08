aa885e862003c1849fa8469b421010f6224e0e38
wuhy <sparklewhy@gmail.com>
2018-11-08 14:31:00 +0800

    test(okam-core): up test spec

--- packages/okam-core/test/tasks/swan/adapter/component.spec.js
+++ packages/okam-core/test/tasks/swan/adapter/component.spec.js
@@ -39,6 +39,31 @@ describe('Swan component adapter', function () {
         });
     });
 
+    it('should trigger event when none detail data', function () {
+        let component = {
+        };
+        componentAdapter(component);
+
+        let spyTriggerEvent = createSpy(() => {});
+        component.methods.triggerEvent = spyTriggerEvent;
+        component.methods.$emit('test');
+        expect(spyTriggerEvent).toHaveBeenCalled();
+        assert(spyTriggerEvent.calls.length === 1);
+        assert(spyTriggerEvent.calls[0].arguments[0] === 'test');
+        expect(spyTriggerEvent.calls[0].arguments[1]).toEqual({
+            type: 'test',
+            currentTarget: {
+                dataset: {},
+                id: undefined
+            },
+            target: {
+                dataset: {},
+                id: undefined
+            },
+            detail: {}
+        });
+    });
+
     it('should not override $emit API if existed in component context', function () {
         let spyEmit = createSpy(() => {});
         let component = {
