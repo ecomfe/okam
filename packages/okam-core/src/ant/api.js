1a7938390437fa3a6779ede3bab7ad81330644e3
wuhy <sparklewhy@gmail.com>
2018-11-22 15:02:59 +0800

    feat(okam-core): add SDKVersion property for getSystemInfo API for ant app

--- packages/okam-core/src/ant/api.js
+++ packages/okam-core/src/ant/api.js
@@ -26,5 +26,50 @@ export default {
         }
 
         my.showToast(options);
+    },
+
+    /**
+     * Get system information async.
+     * Add missing sdk version info to response information
+     *
+     * @param {Object} options the options
+     * @param {Function=} options.success the request success callback
+     * @param {Function=} options.fail the request fail callback
+     * @param {Function=} options.complete the request done callback whatever is
+     * @return {*}
+     */
+    getSystemInfo(options) {
+        let formatData = function (res) {
+            res.SDKVersion = my.SDKVersion;
+        };
+
+        let opts = Object.assign({}, options);
+        let {success: oldSuccess, complete: oldComplete} = opts;
+        oldSuccess && (opts.success = res => {
+            formatData(res);
+            oldSuccess(res);
+        });
+        oldComplete && (opts.complete = res => {
+            if (res && typeof res === 'object' && res.version) {
+                formatData(res);
+            }
+            oldComplete(res);
+        });
+        return my.getSystemInfo(opts);
+    },
+
+    /**
+     * Get system information sync.
+     * Add missing sdk version info to response information
+     *
+     * @return {Object}
+     */
+    getSystemInfoSync() {
+        let res = my.getSystemInfoSync();
+        if (res && typeof res === 'object' && res.version) {
+            res.SDKVersion = my.SDKVersion;
+        }
+
+        return res;
     }
 };
