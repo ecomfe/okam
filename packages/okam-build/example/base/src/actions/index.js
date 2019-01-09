7f6eef5c1521124962b372ae9ae200ed95c350e2
wuhy <sparklewhy@gmail.com>
2019-01-09 16:46:36 +0800

    chore(okam-build): up example

--- packages/okam-build/example/base/src/actions/index.js
+++ packages/okam-build/example/base/src/actions/index.js
@@ -4,4 +4,6 @@
  */
 
 import * as todoActions from './todo';
-export default todoActions;
+
+export default Object.assign({}, todoActions)
+// export default todoActions; // 这种写法 支付宝小程序编译会出错
--- packages/okam-build/example/base/src/common/behavior/util.js
+++ packages/okam-build/example/base/src/common/behavior/util.js
@@ -1,1 +1,4 @@
 
+export function hi() {
+
+}
