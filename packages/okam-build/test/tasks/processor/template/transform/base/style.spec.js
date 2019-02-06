07e74f837c23a64130a93f3b5ffaff1ce85f8906
wuhy <sparklewhy@gmail.com>
2019-02-07 00:03:01 +0800

    test(okam-build): up test spec

--- packages/okam-build/test/tasks/processor/template/transform/base/style.spec.js
+++ packages/okam-build/test/tasks/processor/template/transform/base/style.spec.js
@@ -56,7 +56,7 @@ describe('style binding', function () {
             '<view style="color:{{colorStyle[item]}};font-size:{{fontStyle + \'px\'}};font-weight:{{\'bold\'}}"></view>');
     });
 
-    it.only('should support object abbrev style binding syntax', function () {
+    it('should support object abbrev style binding syntax', function () {
         const file = {
             content: '<div :style="{height, fontSize}"></div>'
         };
