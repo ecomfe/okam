358192e114fb6360cc7b54033390c2d815e39588
wuhy <sparklewhy@gmail.com>
2019-02-12 17:01:55 +0800

    feat(okam-build): add default html tag transform map export api

--- packages/okam-build/index.js
+++ packages/okam-build/index.js
@@ -5,7 +5,11 @@
 
 'use strict';
 
+const {h5ToMiniProgram, miniProgramToH5} = require('./lib/util/tag-map');
+
 // 用于 `component.template.transformTags` 配置项
-exports.reverseTagMap = require('./lib/util/tag-tansform-helper');
+exports.reverseTagMap = require('./lib/util/tag-transform-helper');
+exports.defaultH5TagToMiniProgram = h5ToMiniProgram;
+exports.defaultMiniProgramTagToH5 = miniProgramToH5;
 exports.merge = require('./lib/util').merge;
 exports.run = require('./lib/build');
