3806d0e1f319b80b6952188877483c9cefcef477
wuhy <sparklewhy@gmail.com>
2019-03-06 10:52:50 +0800

    wip(okam-component-h5): add view component

---
+++ packages/okam-component-h5/src/View.vue
@@ -0,0 +1,21 @@
+<template>
+    <div @click="onClick"><slot></slot></div>
+</template>
+<script>
+import animation from './mixins/animation';
+import getHoverMixin from './mixins/hover';
+
+export default {
+    mixins: [getHoverMixin({
+        defaultHoverStartTime: 50,
+        defaultHoverStayTime: 400
+    }), animation],
+
+    methods: {
+        onClick(e) {
+            this.$emit('click', e);
+        }
+    }
+}
+</script>
+
