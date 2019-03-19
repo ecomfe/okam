061daa2bbff53ae618268256ddd8bb2f894ee3f7
wuhy <sparklewhy@gmail.com>
2019-03-19 14:15:30 +0800

    wip(okam-core): add h5 tabbar support

--- packages/okam-core/src/h5/App.js
+++ packages/okam-core/src/h5/App.js
@@ -6,6 +6,7 @@
 'use strict';
 
 /* eslint-disable fecs-export-on-declare */
+/* global document:false */
 
 import createApp from '../App';
 import appBase from './base/application';
@@ -14,18 +15,98 @@ import VueRouter from 'vue-router';
 
 const vueAppBase = createApp(appBase);
 
-function vueAppCreator(routes, ...args) {
+let tabBarComponent;
+
+/**
+ * Init router hook
+ *
+ * @inner
+ * @param {Router} router the router instance
+ * @param {Object} tabBar the tabBar config
+ */
+function initRouterHook(router, tabBar) {
+    const tabPagePaths = tabBar.props.list.map(item => {
+        let {pagePath} = item;
+        pagePath = pagePath.replace(/^\.+/, '');
+        if (pagePath.charAt(0) !== '/') {
+            pagePath = `/${pagePath}`;
+        }
+
+        // reset page path
+        item.pagePath = pagePath;
+        return pagePath;
+    });
+
+    let tabBarEle;
+    router.afterEach((to, from, next) => {
+        if (!tabBarEle) {
+            tabBarEle = document.querySelector('.okam-tabbar-container');
+        }
+
+        if (!tabBarEle) {
+            return;
+        }
+
+        let toPath = to.path;
+        let selTabIdx = tabPagePaths.indexOf(toPath);
+        let show = selTabIdx !== -1;
+        if (show) {
+            tabBarComponent && tabBarComponent.setActiveIndex(selTabIdx);
+        }
+        tabBarEle.style.display = show ? 'block' : 'none';
+        tabBarComponent && (tabBarComponent.show = show);
+    });
+}
+
+function vueAppCreator({routes, tabBar}, ...args) {
+    const router = new VueRouter({
+        mode: 'hash',
+        routes: routes
+    });
+
+    tabBar && initRouterHook(router, tabBar);
+
     new Vue(Object.assign({
         el: '#app',
-        router: new VueRouter({
-            mode: 'hash',
-            routes: routes
-        }),
+        router,
+
+        mounted() {
+            tabBarComponent = this.$refs.tabBar;
+        },
+
         render(createElement) {
-            let children = [createElement('router-view')];
+            let children;
+            if (tabBar) {
+                let tabBarProps = tabBar.props;
+                let isTopPosition = tabBarProps.position === 'top';
+
+                let routerViewEle = createElement('router-view');
+                children = [
+                    createElement('div', {
+                        attrs: {
+                            class: 'okam-tabbar-content'
+                        }
+                    }, [routerViewEle])
+                ];
+
+                children[isTopPosition ? 'unshift' : 'push'](
+                    createElement(
+                        tabBar.creator,
+                        {
+                            props: tabBarProps,
+                            ref: 'tabBar'
+                        }
+                    )
+                );
+            }
+            else {
+                children = [createElement('router-view')];
+            }
+
             return createElement('div', {
                 attrs: {
-                    id: 'app'
+                    id: 'app',
+                    class: 'okam-tabbar-wrap'
                 }
             }, children);
         }
