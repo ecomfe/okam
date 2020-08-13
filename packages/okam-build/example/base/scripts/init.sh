9ebcae6bb66cd74ce0d657a3d007c29e0c79847a
liujiaor <liujiaor@gmail.com>
2020-08-13 21:36:22 +0800

    fix: vue loader version mismatch of h5

--- packages/okam-build/example/base/scripts/init.sh
+++ packages/okam-build/example/base/scripts/init.sh
@@ -4,3 +4,4 @@ ln -nsf ../../../../okam-component node_modules
 ln -nsf ../../../../okam-component-h5 node_modules
 ln -nsf ../../../../okam-api-h5 node_modules
 ln -nsf ../../../../okam-build-h5 node_modules
+ln -nsf ../../../../okam-helper node_modules
